a1dab8b2eea94956c8cf69650fa3f57a32b97bc4
Tyrin Holloway <tychnk@gmail.com>
2023-06-24 13:32:19 -0500

    file updates and config changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["latin"], variable: "--font-mont" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </main>
+  );
 }
